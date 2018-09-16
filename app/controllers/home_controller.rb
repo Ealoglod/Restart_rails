8b1c160430d39062e5f33bd90c81ba6252f169df
Ealoglod <zwinzwin@snu.ac.kr>
2018-09-16 20:08:46 +0900

    09.16

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,24 @@
 class HomeController < ApplicationController
+
+    def index
+        @feeds = Feed.all
+    end
+
+    def new
+    end
+
+    def create
+        feed = Feed.new
+        feed.title = params[:title]
+        feed.content = params[:content]
+        feed.save
+
+        redirect_to action: 'index'
+    end
+
+    def destroy
+        Feed.destroy(params[:id])
+        redirect_to action: 'index'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/feeds' => 'home#index'
+  get '/feeds/new' => 'home#new'
+  post '/feeds' => 'home#create'
+  delete 'feeds/:id' => 'home#destory'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
